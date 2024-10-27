95280280583c60034950106f5b388f7b04f3f68e
Chuks <63956312+skido600@users.noreply.github.com>
2024-10-27 15:16:26 +0100

    tired

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Nav from "./Components/Nav";
 import Hero from "./Components/Hero";
@@ -10,14 +7,9 @@ function App() {
   return (
     <>
       <Nav />
-<<<<<<< HEAD
       <Hero />
-=======
-      
->>>>>>> f5fdf645a4e79bac6cfcf8c0424820a3d74d248e
     </>
   );
 }
 
 export default App;
-// Test
